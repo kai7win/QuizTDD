efb5c02ba7536d0e59edec4ed9d36057116e70ad
Thomas <34007164+kai7win@users.noreply.github.com>
2023-08-15 13:08:51 +0800

    test_startAndAnswerFirstQuestion_withTwoQuestions_routesToSecondQuestion

---
+++ QuizEngine/QuizEngine/Flow.swift
@@ -0,0 +1,38 @@
+//
+//  Flow.swift
+//  QuizEngine
+//
+//  Created by Kai Chi Tsao on 2023/8/15.
+//
+
+import Foundation
+
+protocol Router {
+    func routeTo(question:String,answerCallback:@escaping(String) -> Void)
+}
+
+class Flow{
+    let router:Router
+    let questions:[String]
+    
+    init(questions:[String],router: Router) {
+        self.questions = questions
+        self.router = router
+    }
+    
+    func start(){
+        
+        if let firstQuestion = questions.first{
+            router.routeTo(question: firstQuestion) {[weak self] _ in
+                
+                guard let strongSelf = self else { return }
+                
+                let firstQuestionIndex = strongSelf.questions.firstIndex(of: firstQuestion)!
+                let nextQuestion = strongSelf.questions[firstQuestionIndex + 1]
+                strongSelf.router.routeTo(question: nextQuestion) { _ in }
+            }
+        }
+        
+    }
+    
+}
