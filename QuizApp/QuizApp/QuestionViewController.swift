e9349065f9a29e3b23c72bedc999612da3a3c864
Thomas <34007164+kai7win@users.noreply.github.com>
2023-09-02 15:25:20 +0800

    S01E08: [TDD 🍅 7] Testing Table Views in Swift

--- QuizApp/QuizApp/QuestionViewController.swift
+++ QuizApp/QuizApp/QuestionViewController.swift
@@ -16,10 +16,10 @@ class QuestionViewController:UIViewController,UITableViewDataSource,UITableViewD
     
     private var question = ""
     private var options = [String]()
-    private var selection:((String) -> Void)? = nil
+    private var selection:(([String]) -> Void)? = nil
     private let reuseIdentifier = "Cell"
     
-    convenience init(question:String,options:[String],selection: @escaping (String) -> Void){
+    convenience init(question:String,options:[String],selection: @escaping ([String]) -> Void){
         self.init()
         self.question = question
         self.options = options
@@ -41,7 +41,18 @@ class QuestionViewController:UIViewController,UITableViewDataSource,UITableViewD
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selection?(options[indexPath.row])
+        selection?(selectedOptions(in:tableView))
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if tableView.allowsMultipleSelection{
+            selection?(selectedOptions(in:tableView))
+        }
+    }
+    
+    private func selectedOptions(in tableView:UITableView) -> [String]{
+        guard let indexPaths = tableView.indexPathsForSelectedRows else { return [] }
+        return indexPaths.map{ options[$0.row] }
     }
     
     private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
