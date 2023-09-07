230d3c8b24a91fa8435c32bdb9b85edc37ac45aa
Thomas <34007164+kai7win@users.noreply.github.com>
2023-09-07 13:35:48 +0800

    S01E09: [TDD 🍅 8] Establishing a Presentation Layer in Swift

---
+++ QuizApp/QuizAppTests/ResultsViewControllerTest.swift
@@ -0,0 +1,52 @@
+//
+//  ResultsViewControllerTest.swift
+//  QuizAppTests
+//
+//  Created by Thomas on 2023/9/7.
+//
+
+import Foundation
+import XCTest
+@testable import QuizApp
+
+class ResultsViewControllerTest:XCTestCase{
+    
+    func test_viewDidload_renderSummary(){
+        
+        XCTAssertEqual(makeSUT(summary:"a summary").headerLabel.text, "a summary")
+    }
+    
+    
+    func test_viewDidLoad_rendersAnswers(){
+        XCTAssertEqual(makeSUT(answers: []).tableView.numberOfRows(inSection: 0),0)
+        XCTAssertEqual(makeSUT(answers: [makeDummyAnswer()]).tableView.numberOfRows(inSection: 0),1)
+    }
+    
+    func test_viewDidLoad_withCorrectAnswer_renderCorrectAnswerCell(){
+        let sut = makeSUT(answers: [PresentableAnswer(isCorrect: true)])
+        let cell = sut.tableView.cell(at: 0) as? CorrectAnswerCell
+
+        XCTAssertNotNil(cell)
+    }
+
+    
+    func test_viewDidLoad_withWrongAnswer_renderWrongAnswerCell(){
+        let sut = makeSUT(answers: [PresentableAnswer(isCorrect: false)])
+        let cell = sut.tableView.cell(at: 0) as? WrongAnswerCell
+
+        XCTAssertNotNil(cell)
+    }
+    
+    // MARK: - Helpers
+    
+    func makeSUT(summary:String = "",answers:[PresentableAnswer] = []) -> ResultsViewController{
+        let sut =  ResultsViewController(summary: summary, answers: answers)
+        _ = sut.view
+        return sut
+    }
+    
+    func makeDummyAnswer() -> PresentableAnswer{
+        return PresentableAnswer(isCorrect: false)
+    }
+    
+}
