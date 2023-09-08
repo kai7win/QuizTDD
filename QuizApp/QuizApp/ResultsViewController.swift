e909abfd0c768e01bb2acf6090828870ecb06988
Thomas <34007164+kai7win@users.noreply.github.com>
2023-09-08 08:49:01 +0800

    S01E10: [TDD 🍅 9] Testing Table View Cells in Swift

--- QuizApp/QuizApp/ResultsViewController.swift
+++ QuizApp/QuizApp/ResultsViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 struct PresentableAnswer{
+    let question:String
+    let answer:String
     let isCorrect:Bool
 }
 
 class CorrectAnswerCell:UITableViewCell{
-    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
 }
 
 class WrongAnswerCell:UITableViewCell{
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var correctAnswerLabel: UILabel!
     
 }
 
@@ -38,6 +43,9 @@ class ResultsViewController:UIViewController,UITableViewDataSource{
         super.viewDidLoad()
         
         headerLabel.text = summary
+        tableView.register(UINib(nibName: "CorrectAnswerCell", bundle: nil), forCellReuseIdentifier: "CorrectAnswerCell")
+        tableView.register(UINib(nibName: "WrongAnswerCell", bundle: nil), forCellReuseIdentifier: "WrongAnswerCell")
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,6 +56,19 @@ class ResultsViewController:UIViewController,UITableViewDataSource{
         
         let answer = answers[indexPath.row]
         
-        return answer.isCorrect ? CorrectAnswerCell() : WrongAnswerCell()
+        if answer.isCorrect {
+            return correctAnswerCell(for: answer)
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WrongAnswerCell") as! WrongAnswerCell
+        cell.questionLabel.text = answer.question
+        cell.correctAnswerLabel.text = answer.answer
+        return cell
+    }
+    
+    private func correctAnswerCell(for answer:PresentableAnswer) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CorrectAnswerCell") as! CorrectAnswerCell
+        cell.questionLabel.text = answer.question
+        cell.answerLabel.text = answer.answer
+        return cell
     }
 }
