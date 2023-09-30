eb26dc44a94d30aa083a983b68d7524d50930292
Thomas <34007164+kai7win@users.noreply.github.com>
2023-09-30 16:04:24 +0800

    Remove Question Hashable custom implementation

--- QuizEngine/QuizEngine/Question.swift
+++ QuizEngine/QuizEngine/Question.swift
@@ -10,15 +10,4 @@ import Foundation
 public enum Question<T:Hashable>:Hashable{
     case singleAnswer(T)
     case multipleAnswer(T)
-    
-    public var hashValue: Int{
-        switch self{
-        case .singleAnswer(let value):
-            return "singleAnswer".hashValue ^ value.hashValue
-        case .multipleAnswer(let value):
-            return "multipleAnswer".hashValue ^ value.hashValue
-        }
-    }
-    
-
 }
