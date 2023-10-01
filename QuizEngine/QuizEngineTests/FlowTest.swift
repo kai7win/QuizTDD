61f0635d6910627550300575695c4760bb55fff8
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-01 08:07:14 +0800

    spy helper to clarify

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -147,20 +147,20 @@ class FlowTest:XCTestCase{
     
     
     // MARK: - Helpers
-    private let delegate = RouterSpy()
+    private let delegate = DelegateSpy()
     
-    private weak var weakSUT:Flow<RouterSpy>?
+    private weak var weakSUT:Flow<DelegateSpy>?
     
     
     override func tearDown() {
         super.tearDown()
         
-        XCTAssertNil(weakSUT)
+        XCTAssertNil(weakSUT,"Memory leak detected. Weak reference to the SUT (Flow<DelegateSpy> instance is not nil)")
     }
     
     private func makeSUT(questions:[String],
                  scoring:@escaping ([String:String]) -> Int =  { _ in 0 }
-    ) -> Flow<RouterSpy>{
+    ) -> Flow<DelegateSpy>{
         
         let sut = Flow(questions: questions,router:delegate,scoring: scoring)
         
@@ -170,7 +170,7 @@ class FlowTest:XCTestCase{
     }
     
     
-    private class RouterSpy: Router {
+    private class DelegateSpy: Router {
         var routedQuestions: [String] = []
         var routedResult: Result<String, String>? = nil
         
