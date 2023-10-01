26164f1676c3bc14ded4c5a6dee709acbf95bcac
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-01 08:16:59 +0800

    Renamed spy property to clarify intent

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -87,12 +87,12 @@ class FlowTest:XCTestCase{
         
         makeSUT(questions:[]).start()
         
-        XCTAssertEqual(delegate.routedResult!.answers,[:])
+        XCTAssertEqual(delegate.handledResult!.answers,[:])
     }
     
     func test_start_withOneQuestion_doesNotRouteToResult(){
         makeSUT(questions:["Q1"]).start()
-        XCTAssertNil(delegate.routedResult)
+        XCTAssertNil(delegate.handledResult)
     }
     
     func test_startAndAnswerFirst_withTwoQuestions_doesNotRoutesToResult(){
@@ -103,7 +103,7 @@ class FlowTest:XCTestCase{
         sut.start()
         delegate.answerCallback("A1")
         
-        XCTAssertNil(delegate.routedResult)
+        XCTAssertNil(delegate.handledResult)
     }
     
     
@@ -116,7 +116,7 @@ class FlowTest:XCTestCase{
         delegate.answerCallback("A1")
         delegate.answerCallback("A2")
         
-        XCTAssertEqual(delegate.routedResult!.answers, ["Q1":"A1","Q2":"A2"])
+        XCTAssertEqual(delegate.handledResult!.answers, ["Q1":"A1","Q2":"A2"])
     }
     
     
@@ -128,7 +128,7 @@ class FlowTest:XCTestCase{
         delegate.answerCallback("A1")
         delegate.answerCallback("A2")
         
-        XCTAssertEqual(delegate.routedResult!.score, 10)
+        XCTAssertEqual(delegate.handledResult!.score, 10)
     }
     
     func test_startAndAnswerFirstAndSecondQuestion_withTwoQuestions_scoresWithRightAnswers(){
@@ -173,7 +173,7 @@ class FlowTest:XCTestCase{
     private class DelegateSpy: Router,QuizDelegate {
         
         var handledQuestions: [String] = []
-        var routedResult: Result<String, String>? = nil
+        var handledResult: Result<String, String>? = nil
         
         var answerCallback: (String) -> Void = { _ in }
         
@@ -188,7 +188,7 @@ class FlowTest:XCTestCase{
         
         
         func handle(result: Result<String, String>) {
-            routedResult = result
+            handledResult = result
         }
         
         func routeTo(result: Result<String, String>) {
