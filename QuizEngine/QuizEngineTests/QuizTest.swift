7ad2c07041d203d7ac613ba5526c4d208e0945f3
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-04 07:54:19 +0800

    Capture answer completions in a ordered form

--- QuizEngine/QuizEngineTests/QuizTest.swift
+++ QuizEngine/QuizEngineTests/QuizTest.swift
@@ -19,12 +19,26 @@ class QuizTest: XCTestCase {
         
         quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate)
         
-        delegate.answerCompletion("A1")
-        delegate.answerCompletion("A2")
+        delegate.answerCompletions[0]("A1")
+        delegate.answerCompletions[1]("A2")
         
         XCTAssertEqual(delegate.completedQuizzes.count, 1)
         assertEqual(delegate.completedQuizzes[0], [("Q1","A1"),("Q2","A2")])
     }
+    
+//    func test_startQuiz_answersAllQuestionsTwice_completesWithNewAnswers() {
+//
+//        quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate)
+//
+//        delegate.answerCompletion[0]("A1")
+//        delegate.answerCompletion[1]("A2")
+//        delegate.answerCompletion("A1-1")
+//        delegate.answerCompletion("A2-2")
+//
+//        XCTAssertEqual(delegate.completedQuizzes.count, 2)
+//        assertEqual(delegate.completedQuizzes[0], [("Q1","A1"),("Q2","A2")])
+//        assertEqual(delegate.completedQuizzes[1], [("Q1","A1-1"),("Q2","A2-2")])
+//    }
     
     private func assertEqual(_ a1:[(String,String)],_ a2:[(String,String)],file:StaticString = #file,line:UInt = #line){
         XCTAssertTrue(a1.elementsEqual(a2, by: ==),"\(a1) is not equal to \(a2)",file: file,line: line)
@@ -34,10 +48,10 @@ class QuizTest: XCTestCase {
     private class DelegateSpy: QuizDelegate {
         
         var completedQuizzes:[[(String,String)]] = []
-        var answerCompletion: (String) -> Void = { _ in }
+        var answerCompletions: [(String) -> Void] = []
         
         func answer(for question: String, completion: @escaping (String) -> Void) {
-            self.answerCompletion = completion
+            self.answerCompletions.append(completion)
         }
        
         func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
