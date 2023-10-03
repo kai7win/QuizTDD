f46a2ff364cddad18f15fbb245357d2e31ed3b8e
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-03 17:04:05 +0800

    Improved test readability with helper function

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -119,9 +119,10 @@ class FlowTest:XCTestCase{
         delegate.answerCompletion("A2")
         
         XCTAssertEqual(delegate.completedQuizzes.count,1)
-        XCTAssertTrue(delegate.completedQuizzes[0].elementsEqual( [("Q1","A1"),("Q2","A2")], by: ==))
+        
+        assertEqual(delegate.completedQuizzes[0], [("Q1","A1"),("Q2","A2")])
+        
     }
-    
     
     func test_startAndAnswerFirstAndSecondQuestion_withTwoQuestions_scores(){
         
@@ -172,6 +173,10 @@ class FlowTest:XCTestCase{
         return sut
     }
     
+    
+    private func assertEqual(_ a1:[(String,String)],_ a2:[(String,String)],file:StaticString = #file,line:UInt = #line){
+        XCTAssertTrue(a1.elementsEqual(a2, by: ==),"\(a1) is not equal to \(a2)",file: file,line: line)
+    }
     
     private class DelegateSpy: QuizDelegate {
         
