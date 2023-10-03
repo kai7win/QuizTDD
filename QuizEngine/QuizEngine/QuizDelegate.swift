9b8de9752a9b73053a377d38685b4c60c26a35b9
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-03 13:46:04 +0800

    Deprecated the handle(result:) API in favor of the new didCompleteQuiz(withAnswers)

--- QuizEngine/QuizEngine/QuizDelegate.swift
+++ QuizEngine/QuizEngine/QuizDelegate.swift
@@ -12,7 +12,14 @@ public protocol QuizDelegate{
     associatedtype Answer
     
     func answer(for question:Question,completion:@escaping (Answer) -> Void)
+    
+    func didCompleteQuiz(withAnswers:[(question:Question,answer:Answer)])
+    
+    @available(*,deprecated,message: "use didCompleteQuiz(withAnswers:) instead")
     func handle(result:Result<Question,Answer>)
 }
 
-
+#warning("Delete this at some point")
+public extension QuizDelegate{
+    func didCompleteQuiz(withAnswers:[(question:Question,answer:Answer)]) {}
+}
