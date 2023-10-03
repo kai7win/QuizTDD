0ce25d05f47f4a10faa2bba374a97456c03c91a6
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-03 13:34:13 +0800

    Improved a name to clarify intent

--- QuizEngine/QuizEngineTests/QuizTest.swift
+++ QuizEngine/QuizEngineTests/QuizTest.swift
@@ -22,24 +22,24 @@ class QuizTest: XCTestCase {
     
     func test_startQuiz_answerOneOutOfTwoCorrectly_scoresZero() {
         
-        delegate.answerCallback("wrong")
-        delegate.answerCallback("wrong")
+        delegate.answerCompletion("wrong")
+        delegate.answerCompletion("wrong")
         
         XCTAssertEqual(delegate.handleResult!.score, 0)
     }
     
     func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
         
-        delegate.answerCallback("A1")
-        delegate.answerCallback("wrong")
+        delegate.answerCompletion("A1")
+        delegate.answerCompletion("wrong")
         
         XCTAssertEqual(delegate.handleResult!.score, 1)
     }
     
     func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
         
-        delegate.answerCallback("A1")
-        delegate.answerCallback("A2")
+        delegate.answerCompletion("A1")
+        delegate.answerCompletion("A2")
         
         XCTAssertEqual(delegate.handleResult!.score, 2)
     }
@@ -48,10 +48,10 @@ class QuizTest: XCTestCase {
         
         var handleResult: Result<String, String>? = nil
         
-        var answerCallback: (String) -> Void = { _ in }
+        var answerCompletion: (String) -> Void = { _ in }
         
         func answer(for: String, completion: @escaping (String) -> Void) {
-            self.answerCallback = completion
+            self.answerCompletion = completion
         }
         
         func handle(result: Result<String, String>) {
