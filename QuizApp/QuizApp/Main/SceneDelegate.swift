7266d7459d5e4be9b8b69765e1326c484f955f95
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-09 05:17:59 +0800

    Migrated the SceneDelegate to new Quiz API

--- QuizApp/QuizApp/Main/SceneDelegate.swift
+++ QuizApp/QuizApp/Main/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
 
     var window: UIWindow?
-    var game:Game<Question<String>,[String],NavigationControllerRouter>?
+    var quiz:Quiz?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
@@ -31,13 +31,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let option6 = "Brazilian"
         let options2 = [option4,option5,option6]
 
-        let correctAnswers = [question1:[option3],question2:[option4,option6]]
+        let options = [question1:options1,question2:options2]
+        
+        let correctAnswers = [(question1,[option3]),(question2,[option4,option6])]
 
         let navigationController = UINavigationController()
-        let factory = iOSViewControllerFactory(
-            options: [question1:options1,question2:options2],
-            correctAnswers: [(question1,[option3]),(question2,[option4,option6])]
-        )
+        let factory = iOSViewControllerFactory(options: options, correctAnswers: correctAnswers)
         let router = NavigationControllerRouter(navigationController, factory: factory)
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -47,7 +46,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
 
-        game = startGame(questions: questions, router: router, correctAnswers: correctAnswers)
+        quiz = Quiz.start(questions: questions, delegate: router)
 
     }
     
