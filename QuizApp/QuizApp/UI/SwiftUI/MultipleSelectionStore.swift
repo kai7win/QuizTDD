5f7923ded70254aa743e9a0f82bb0329109bcb17
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-10 06:03:26 +0800

    Move MultipleSelectionStore to prod target

---
+++ QuizApp/QuizApp/UI/SwiftUI/MultipleSelectionStore.swift
@@ -0,0 +1,38 @@
+//
+//  MultipleSelectionStore.swift
+//  QuizApp
+//
+//  Created by Thomas on 2023/10/10.
+//
+
+import Foundation
+
+struct MultipleSelectionStore{
+    
+    var options:[MultipleSelectionOption]
+    var canSubmit:Bool {
+        !options.filter(\.isSelected).isEmpty
+    }
+    
+    private let handler:([String]) -> Void
+    
+    internal init(options: [String],handler:@escaping ([String]) -> Void = { _ in }) {
+        self.options = options.map { MultipleSelectionOption(text: $0) }
+        self.handler = handler
+    }
+    
+    func submit(){
+        guard canSubmit else { return }
+        handler(options.filter(\.isSelected).map(\.text))
+    }
+    
+}
+
+struct MultipleSelectionOption{
+    let text:String
+    var isSelected = false
+    
+    mutating func select(){
+        isSelected.toggle()
+    }
+}
