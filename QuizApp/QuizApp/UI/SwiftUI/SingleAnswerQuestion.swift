2787837113dcf2df1e43aab19d447aef797b5205
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-09 13:52:41 +0800

    Add interactable SingleAnswerQuestionTestView preview

--- QuizApp/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ QuizApp/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -20,7 +20,9 @@ struct SingleAnswerQuestion: View {
             QuestionHeader(title: title, question: question)
             
             ForEach(options,id:\.self){ option in
-                SingleTextSelectionCell(text: option, selection: { })
+                SingleTextSelectionCell(text: option, selection: {
+                    selection(option)
+                })
             }
             
             Spacer()
@@ -30,16 +32,32 @@ struct SingleAnswerQuestion: View {
 
 struct SingleAnswerQuestion_Previews: PreviewProvider {
     static var previews: some View {
-        SingleAnswerQuestion(
-            title: "1 of 2",
-            question: "What's Mike's nationality?",
-            options: [
-                "Portuguese",
-                "American",
-                "Greek",
-                "Canadian"
-            ],
-            selection: { _ in })
+        
+        SingleAnswerQuestionTestView()
+        
+    }
+    
+    struct SingleAnswerQuestionTestView:View{
+        
+        @State var selection:String = "none"
+        
+        var body: some View{
+            VStack{
+                SingleAnswerQuestion(
+                    title: "1 of 2",
+                    question: "What's Mike's nationality?",
+                    options: [
+                        "Portuguese",
+                        "American",
+                        "Greek",
+                        "Canadian"
+                    ],
+                    selection: { selection = $0 })
+                
+                Text("Last selection" + selection)
+            }
+        }
+        
     }
 }
 
