52fb2a9d5f8333b6e2c4aadc00678a5c8e92beea
Thomas <34007164+kai7win@users.noreply.github.com>
2023-10-09 16:36:34 +0800

    Implement new question title format

--- QuizApp/QuizApp/Presentation/Question/QuestionPresenter.swift
+++ QuizApp/QuizApp/Presentation/Question/QuestionPresenter.swift
@@ -15,6 +15,6 @@ struct QuestionPresenter{
     
     var title:String{
         guard let index = questions.firstIndex(of:question) else { return "" }
-        return "Question #\(index + 1)"
+        return "\(index + 1) of \(questions.count)"
     }
 }
